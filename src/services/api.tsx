023c6963aeae957bdb746eee28dc3f171c6144f4
ankitagutte <ankitagutte2007@gmail.com>
2024-06-22 11:08:08 +0530

    first commit

---
+++ src/services/api.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+export const BASE_URL = 'https://newsapi.org/v2/';
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getHeadlineByCountry = async () => {
+  axios
+    .get(
+      'https://newsapi.org/v2/top-headlines?country=in&apiKey=f463419c4e4c4ebd96549c95688e979b'
+    )
+    .then((response: any) => {
+      // handle success
+      return response.data;
+    })
+    .catch((error: any) => {
+      // handle error
+      console.log(error);
+    });
+};
+
+export const fetchSearchDataApi = async (query: any) => {
+  const API_KEY = '61f0d623ef4f4b50b8ecdb9acdab4058';
+ 
+  try {
+    const data = await axios.get(
+      ` https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=${API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
