328e72d52998fea3584fe0b82e8269201e9d22ea
ankitagutte <ankitagutte2007@gmail.com>
2024-06-22 13:02:02 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,31 @@
 
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',  // Bind to all network interfaces if needed
+//     port: 5173,        // Set the port number
+//   },
+// });
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Bind to all network interfaces
-    port: 5173       // Set the port number
-  }
-})
+  build: {
+    minify: true,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+});
+
